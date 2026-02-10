d5a1d3bc06366f42f79c412e404b12d517f92b63
MatheusABA <matheus.alencar02199@gmail.com>
2026-02-10 17:15:57 -0400

    new - Initial project

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { Elysia } from "elysia";
+import { z } from "zod";
+import openapi from "@elysiajs/openapi";
+
+const MainServer = new Elysia()
+  .get("/", () => "Hello Elysia\n")
+  .get("/users/:id", ({ params }: any) => {
+    const userId = params.id;
+
+    return { id: userId, name: 'Matheus Alexandre' }
+  }, {
+    params: z.object({
+      id: z.string()
+    }),
+    detail: {
+      summary: "Get user by ID",
+      tags: ["User"]
+    },
+    response: {
+      200: z.object({
+        id: z.string(),
+        name: z.string()
+      }),
+      404: z.object({
+        message: z.string()
+      })
+    }
+  })
+  .use(openapi())
+  .listen(4040);
+
+
+console.log(
+  `🦊 Elysia is running at http://${MainServer.server?.hostname}:${MainServer.server?.port}`
+);
